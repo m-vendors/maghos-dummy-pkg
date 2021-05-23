e3fd91462a2ed55a927a18e08e8aa81428eb10e3
M Vendors <m-vendors@protonmail.com>
2021-05-23 22:17:59 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'maghos_DummyPkg',
+    __DIR__
+);
